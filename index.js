fd18da33f7e096093a82a5b52daf8a85c21d9b1e
Jeff Mealo <jeffreymealo@gmail.com>
2020-06-22 10:13:58 -0400

    fix: use jsonPointers: true for better formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Ajv = require('ajv')
-const ajv = new Ajv({ removeAdditional: true, useDefaults: true, coerceTypes: true })
+const ajv = new Ajv({ removeAdditional: true, useDefaults: true, coerceTypes: true, jsonPointers: true })
 
 ajv.addKeyword('separator', {
   type: 'string',
